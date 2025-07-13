119c5382628e4f5264c1e24ebbbddc2c335a3070
shaianeviana <shai_ane@hotmail.com>
2025-07-13 20:00:46 -0300

    favico.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/images/favicon.jpg" />
+        <link rel="icon" href="/images/favicon.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
